e7de9b9e540ca145913fb13dd00f29e37a150db2
sangryul <ksl970330@naver.com>
2021-08-21 06:57:59 -0700

    add crawling on linux and models with bert and gpt2

--- src/crawling_mbticafe.py
+++ src/crawling_mbticafe.py
@@ -6,6 +6,7 @@
 # In[1]:
 
 
+from os import error
 import time
 import pandas as pd
 import pickle
@@ -32,7 +33,7 @@ def copy_input(xpath, input):
 # In[3]:
 
 
-driver = webdriver.Chrome('chromedriver.exe')
+driver = webdriver.Chrome('/home/sangryul/Desktop/chromedriver')
 driver.implicitly_wait(3)
 
 
@@ -60,30 +61,35 @@ driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
 
 def crawling_address_and_nickname(menuid):
-    page = 0
+    page = 400
     address = []
     nickname = []
 
-    while page < -1:
+    while page < 1000:
+        #page += 1
         try:
             # 페이지 이동
             page += 1
             driver.get(f"https://cafe.naver.com/ArticleList.nhn?search.clubid=11856775&search.menuid={menuid}&search.boardtype=L&search.page={page}")
-            driver.switch_to.frame("cafe_main")
-
-            
+            try:
+                driver.switch_to.frame("cafe_main")
+            except:
+                continue
 
             # 게시글 없을 경우
             try:
                 if driver.find_element_by_class_name("nodata").text == '등록된 게시글이 없습니다.':
                     break
+                    pass
             except:
+                print("no article")
+                #print(error)
                 pass
   
 
 
             # 글 주소, 작성자 닉네임 크롤링
-            for i in range(1, 51, 1):
+            for i in range(1, 16, 1):
                 cur_address = driver.find_element_by_xpath(f"/html/body/div[1]/div/div[4]/table/tbody/tr[{i}]/td[1]/div[2]/div/a[1]").get_attribute("href")
                 cur_nickname = driver.find_element_by_xpath(f"/html/body/div[1]/div/div[4]/table/tbody/tr[{i}]/td[2]/div/table/tbody/tr/td/a").get_attribute("onclick")
                 address.append(cur_address)
@@ -192,9 +198,14 @@ def crawling(menuid, mbti_group):
     title, content = crawling_title_and_content(address)
     print('  progress time: {} seconds \n'.format(time.time() - start_time))
     
+    print(len(nickname), len(title), len(content))
     
     
-    data = pd.DataFrame({'mbti': nickname, 'title': title, 'content': content})
+    #data = pd.DataFrame({'mbti': nickname, 'title': title, 'content': content})
+    json_data = {'mbti': nickname, 'title': title, 'content': content}
+    data = pd.DataFrame.from_dict(json_data, orient='index')
+    data = data.transpose()
+    print(data)
     return data
 
 
@@ -214,85 +225,85 @@ mbti_group = ['intj', 'intp', 'entj', 'entp',
 
 
 # INFP & ENFP 게시판 크롤링, menuid=18
-data18 = crawling(18, mbti_group)
-data18.head(5)
+# data18 = crawling(18, mbti_group)
+# data18.head(5)
 
 
-# In[12]:
+# # In[12]:
 
 
-data18.to_pickle('menuid=18 crawling.pkl')
+# data18.to_pickle('menuid=18 crawling2.pkl')
 
-with open("./menuid=18 crawling.pickle", "wb") as MyFile:
-    pickle.dump(data18, MyFile)
+# with open("./data18.pickle", "wb") as MyFile:
+#    pickle.dump(data18, MyFile)
 
 
 # In[13]:
 
 
 # INTP & ENTP 게시판 크롤링, menuid=17
-data17 = crawling(17, mbti_group)
-data17.head(5)
+# data17 = crawling(17, mbti_group)
+# data17.head(5)
 
 
-# In[14]:
+# #In[14]:
 
 
-data17.to_pickle('menuid=17 crawling.pkl')
+# data17.to_pickle('menuid=17 crawling.pkl')
 
-with open("./data17.pickle", "wb") as MyFile:
-    pickle.dump(data17, MyFile)
+# with open("./data17.pickle", "wb") as MyFile:
+#     pickle.dump(data17, MyFile)
 
 
 # In[15]:
 
 
 # ISFP & ESFP 게시판 크롤링, menuid=16
-data16 = crawling(16, mbti_group)
-data16.head(5)
+# data16 = crawling(16, mbti_group)
+# data16.head(5)
 
 
-# In[16]:
+# # In[16]:
 
 
-data16.to_pickle('menuid=16 crawling.pkl')
+# data16.to_pickle('menuid=16 crawling.pkl')
 
-with open("./data16.pickle", "wb") as MyFile:
-    pickle.dump(data16, MyFile)
+# with open("./data16.pickle", "wb") as MyFile:
+#     pickle.dump(data16, MyFile)
 
 
 # In[17]:
 
 
 # ISTP & ESTP 게시판 크롤링, menuid=15
-data15 = crawling(15, mbti_group)
-data15.head(5)
+# data15 = crawling(15, mbti_group)
+# data15.head(5)
 
 
-# In[18]:
+# # In[18]:
 
 
-data15.to_pickle('menuid=15 crawling.pkl')
+# data15.to_pickle('menuid=15 crawling.pkl')
 
-with open("./data15.pickle", "wb") as MyFile:
-    pickle.dump(data15, MyFile)
+# with open("./data15.pickle", "wb") as MyFile:
+#     pickle.dump(data15, MyFile)
 
 
 # In[19]:
 
 
 # INFJ & ENFJ 게시판 크롤링, menuid=14
-data14 = crawling(14, mbti_group)
-data14.head(5)
+# data14 = crawling(14, mbti_group)
+# data14.head(5)
 
 
-# In[20]:
+# # In[20]:
 
 
-data14.to_pickle('menuid=14 crawling.pkl')
+# data14.to_pickle('menuid=14 crawling.pkl')
 
-with open("./data14.pickle", "wb") as MyFile:
-    pickle.dump(data14, MyFile)
+# with open("./data14.pickle", "wb") as MyFile:
+#     pickle.dump(data14, MyFile)
 
 
 # In[21]:
@@ -303,30 +314,30 @@ data13 = crawling(13, mbti_group)
 data13.head(5)
 
 
-# In[22]:
+# # In[22]:
 
 
-data13.to_pickle('menuid=13 crawling.pkl')
+# data13.to_pickle('menuid=13 crawling.pkl')
 
-with open("./data13.pickle", "wb") as MyFile:
-    pickle.dump(data13, MyFile)
+# with open("./data13.pickle", "wb") as MyFile:
+#     pickle.dump(data13, MyFile)
 
 
 # In[23]:
 
 
 # INTJ & ENTJ 게시판 크롤링, menuid=12
-data12 = crawling(12, mbti_group)
-data12.head(5)
+# data12 = crawling(12, mbti_group)
+# data12.head(5)
 
 
-# In[24]:
+# # In[24]:
 
 
-data12.to_pickle('menuid=12 crawling.pkl')
+# data12.to_pickle('menuid=12 crawling.pkl')
 
-with open("./data12.pickle", "wb") as MyFile:
-    pickle.dump(data12, MyFile)
+# with open("./data12.pickle", "wb") as MyFile:
+#     pickle.dump(data12, MyFile)
 
 
 # In[25]:
